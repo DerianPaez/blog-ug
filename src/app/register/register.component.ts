5a54d33cc114140d1ec9690db928be4b3bf70750
James <mendozajames02@hotmail.com>
2024-01-23 22:20:18 -0500

    Mejorar formularios de login y registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,18 +10,27 @@ import { AuthService } from '../services/auth/auth.service';
 export class RegisterComponent {
   user = { name: '', email: '', password: '' };
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit() {
     console.log({ user: this.user });
 
-    this.authService.registerUser(this.user).subscribe(
-      (data) => {
+    this.authService.registerUser(this.user).subscribe({
+      next: (data) => {
         console.log(data);
+        alert('Registro exitoso.');
+        this.router.navigate(['/']);
       },
-      (error) => {
-        console.log(error);
-      }
-    );
+      error: (error) => {
+        console.error(error);
+        alert(
+          `No pudimos completar tu registro. Por favor revisa la información proporcionada y asegúrate de que cumple con todos los requisitos.\n\n${JSON.stringify(
+            error.error,
+            null,
+            2
+          )}`
+        );
+      },
+    });
   }
 }
